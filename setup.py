54e04f20d53ec8e683aab1a73d14ce762bb9d883
Javan Whitney-Warner <67432509+JavanW-W@users.noreply.github.com>
2024-06-11 14:34:08 -0500

    Bump version number for adding pytyped

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as in_file:
 
 setup(
     name='copley',
-    version="0.1.0",
+    version="0.1.1",
     description='Python driver for Copley Tapped Density Tester.',
     long_description=long_description,
     long_description_content_type='text/markdown',
